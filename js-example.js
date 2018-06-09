490bac5a3cee6cf86baefc6870397b37685ccef5
rishikmr051 <40091516+rishikmr051@users.noreply.github.com>
2018-06-09 15:23:44 +0530

    Update js-example.js

--- js-example.js
+++ js-example.js
@@ -1,3 +1,17 @@
 // Higher order function : function that returns another function
 
-fucn
+function parent() {
+ let a =10;
+  return function () {
+    a = a + 1;
+   console.log(a );
+  }
+  
+}
+
+const inner1 = parent();
+innner1();
+
+const inner2 = parent();
+innner2();
+innner1();
